2233099c054b33b33eb044241b4dd33b6679b0ae
Maksym <4ygakk@gmail.com>
2017-03-27 15:34:04 +0200

    first

---
+++ EDwL1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace EDwL1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Words = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Counts = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create file with source code URL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(253, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Delete spaces and \"<\" \">\"";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Paste your URL";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Words,
+            this.Counts});
+            this.listView1.Location = new System.Drawing.Point(12, 139);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(253, 252);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(252, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Count words";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 403);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader Words;
+        private System.Windows.Forms.ColumnHeader Counts;
+    }
+}
+
---
+++ EDwL1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDwL1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urlAddress = textBox1.Text;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding("Windows-1250"));
+                }
+
+                string data = readStream.ReadToEnd();
+                using (FileStream fs = new FileStream("test.htm", FileMode.Create))
+                {
+                    using (StreamWriter w = new StreamWriter(fs, Encoding.GetEncoding("Windows-1250")))
+                    {
+                        w.Write(data);
+                    }
+                }
+                response.Close();
+         
+                readStream.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string textHtml = System.IO.File.ReadAllText("test.htm", Encoding.GetEncoding("Windows-1250"));
+            using (FileStream fs = new FileStream("test.txt", FileMode.Create))
+            {
+                using (StreamWriter w = new StreamWriter(fs, Encoding.GetEncoding("Windows-1250")))
+                {
+                    //textHtml = Regex.Replace(textHtml, @"\s+", "");
+                    textHtml = Regex.Replace(textHtml, @"<[^>]+>", "");
+                    textHtml = Regex.Replace(textHtml, @"[^\w\s]+|[\d]+", "");
+                    //textHtml = Regex.Replace(textHtml, @"[\W]", "");
+                    textHtml = Regex.Replace(textHtml, @"\s+", " ");
+                    w.Write(textHtml);
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var fs = new FileStream("test.txt", FileMode.Open, FileAccess.Read,
+                                      FileShare.ReadWrite | FileShare.Delete);
+            string content;
+            using (StreamReader reader = new StreamReader(fs, Encoding.GetEncoding("Windows-1250")))
+            {
+                content = reader.ReadToEnd();
+            }
+            string[] words = content.Split(' ');
+
+            foreach(string count in words.Distinct())
+            {
+                //listView1.Columns.Add("Word", 20, HorizontalAlignment.Left);
+                //listView1.Columns.Add("Count", 20, HorizontalAlignment.Left);
+                //string[] row = { count + " - " + words.Where(x => x == count).Count() + " times" };
+                //var listViewItem = new ListViewItem(row);
+                //listView1.Items.Add(listViewItem);
+
+
+                listView1.Items.Add(count + " - " + words.Where(x => x == count).Count() + " раз");
+               
+            }
+        }
+    }
+    }
+
